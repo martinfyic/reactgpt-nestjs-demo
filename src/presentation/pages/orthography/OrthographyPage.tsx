cd637676e50739703b7611dd008a0eaec9b3f9de
martinfyic <martin.f.yic@gmail.com>
2024-09-18 19:21:29 -0300

    refactor: :lipstick: actualizo archivo src/presentaion/page/Orthography.tsx

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,3 +1,18 @@
+import { GptMessage, TypingLoader, UserMessage } from '../../components';
+
 export const OrthographyPage = () => {
-	return <div>OrthographyPage</div>;
+	return (
+		<div className='chat-container'>
+			<div className='chat-messages'>
+				<div className='grid grid-cols-12 gap-y-2'>
+					{/* Bienvenida */}
+					<GptMessage text='Hola, escribe tu texto y te ayudo con la corrección' />
+
+					<UserMessage text='Hola Mundo' />
+
+					<TypingLoader className='fade-in' />
+				</div>
+			</div>
+		</div>
+	);
 };
