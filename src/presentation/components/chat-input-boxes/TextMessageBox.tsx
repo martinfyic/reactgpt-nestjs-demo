6b286deb14f3fc50c5b87e3b11ec26a4dd53c579
martinfyic <martin.f.yic@gmail.com>
2024-09-20 11:46:17 -0300

    feat: :lipstick: creo componente src/presentation/components/chat-input-boxes/TextMessageBox.tsx

---
+++ src/presentation/components/chat-input-boxes/TextMessageBox.tsx
@@ -0,0 +1,56 @@
+import { FormEvent, useState } from 'react';
+
+interface Props {
+	onSendMessage: (message: string) => void;
+	placeholder?: string;
+	disableCorrections?: boolean;
+}
+
+export const TextMessageBox = ({
+	onSendMessage,
+	disableCorrections = false,
+	placeholder,
+}: Props) => {
+	const [message, setMessage] = useState('');
+
+	const handleSendMessage = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		if (message.trim().length === 0) return;
+
+		onSendMessage(message);
+		setMessage('');
+	};
+
+	return (
+		<form
+			onSubmit={handleSendMessage}
+			className='flex flex-row items-center h-16 rounded-xl bg-white w-full px-4'
+		>
+			<div className='flex-grow'>
+				<div className='relative w-full'>
+					<input
+						value={message}
+						onChange={e => setMessage(e.target.value)}
+						type='text'
+						name='message'
+						id='message'
+						autoFocus
+						className='flex w-full border rounded-xl text-gray-800 focus:outline-none focus:border-indigo-300 pl-4 h-10'
+						placeholder={placeholder}
+						autoComplete={disableCorrections ? 'on' : 'off'}
+						autoCorrect={disableCorrections ? 'on' : 'off'}
+						spellCheck={disableCorrections ? 'true' : 'false'}
+					/>
+				</div>
+			</div>
+
+			<div className='ml-4'>
+				<button className='btn-primary'>
+					<span className='mr-2'>Enviar</span>
+					<i className='fa-regular fa-paper-plane'></i>
+				</button>
+			</div>
+		</form>
+	);
+};
