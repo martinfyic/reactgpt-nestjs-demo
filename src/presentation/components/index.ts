7a6fe991113cfe07d4a97b032455689d05aef31c
martinfyic <martin.f.yic@gmail.com>
2024-09-20 11:45:11 -0300

    chore: :art: agrego referencia para archivo de exportacion TextMessageBox TextMessageBoxFile

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,6 +1,9 @@
 export * from './chat-bubbles/GptMessage';
 export * from './chat-bubbles/UserMessage';
 
+export * from './chat-input-boxes/TextMessageBox';
+export * from './chat-input-boxes/TextMessageBoxFile';
+
 export * from './loaders/TypingLoader';
 
 export * from './sidebar/SidebarMenuItem';
