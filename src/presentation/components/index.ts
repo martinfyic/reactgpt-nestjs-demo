33b0890c97625ded556526063684c7c2507fcde3
martinfyic <martin.f.yic@gmail.com>
2024-09-21 20:19:24 -0300

    chore: :art: agrego exportacion de TextMessageBoxSelected.tsx en archivo

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -3,6 +3,7 @@ export * from './chat-bubbles/UserMessage';
 
 export * from './chat-input-boxes/TextMessageBox';
 export * from './chat-input-boxes/TextMessageBoxFile';
+export * from './chat-input-boxes/TextMessageBoxSelect';
 
 export * from './loaders/TypingLoader';
 
